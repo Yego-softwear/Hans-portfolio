4adefa1d6d32783aee47b15a4dd1b886ef13e88f
HANS KIPROP YEGO <76849602+Yego-softwear@users.noreply.github.com>
2025-07-25 20:54:36 +0300

    Update script.js
    Signed-off-by: HANS KIPROP YEGO <76849602+Yego-softwear@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+let current = 0;
+const testimonials = document.querySelectorAll('.testimonial');
+
+setInterval(() => {
+  testimonials[current].classList.remove('active');
+  current = (current + 1) % testimonials.length;
+  testimonials[current].classList.add('active');
+}, 4000);
 
